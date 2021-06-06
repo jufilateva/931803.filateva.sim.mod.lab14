30e4af9a2a9b6d7ca8938b2c0d9804ab77a6e2d5
jufilateva <74243446+jufilateva@users.noreply.github.com>
2021-06-06 12:37:48 +0500

    Add files via upload

---
+++ Sim.mod.lab14/Sim.mod.lab14/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Sim.mod.lab14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.b_start = new System.Windows.Forms.Button();
+            this.av = new System.Windows.Forms.Label();
+            this.var = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mean:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Variance:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sample size";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 223);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(323, 62);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series2.ChartArea = "ChartArea1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(477, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 400);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Average:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 444);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Variance:";
+            // 
+            // b_start
+            // 
+            this.b_start.Location = new System.Drawing.Point(35, 298);
+            this.b_start.Name = "b_start";
+            this.b_start.Size = new System.Drawing.Size(244, 55);
+            this.b_start.TabIndex = 11;
+            this.b_start.Text = "Start";
+            this.b_start.UseVisualStyleBackColor = true;
+            this.b_start.Click += new System.EventHandler(this.b_start_Click);
+            // 
+            // av
+            // 
+            this.av.AutoSize = true;
+            this.av.Location = new System.Drawing.Point(117, 400);
+            this.av.Name = "av";
+            this.av.Size = new System.Drawing.Size(32, 17);
+            this.av.TabIndex = 12;
+            this.av.Text = "___";
+            // 
+            // var
+            // 
+            this.var.AutoSize = true;
+            this.var.Location = new System.Drawing.Point(117, 444);
+            this.var.Name = "var";
+            this.var.Size = new System.Drawing.Size(32, 17);
+            this.var.TabIndex = 13;
+            this.var.Text = "___";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 603);
+            this.Controls.Add(this.var);
+            this.Controls.Add(this.av);
+            this.Controls.Add(this.b_start);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button b_start;
+        private System.Windows.Forms.Label av;
+        private System.Windows.Forms.Label var;
+    }
+}
+
---
+++ Sim.mod.lab14/Sim.mod.lab14/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sim.mod.lab14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int N, k;
+        double average, variance;
+        double average_emp, variance_emp;
+        Random r = new Random();
+
+
+        double[] sample; 
+        int[] counter; 
+        double[] Fr; 
+        double[] X;
+
+        public void average_variance(double[] a)
+        {
+            average_emp = a.Average();
+            variance_emp = 0;
+            for (int i = 0; i < N; i++) variance_emp += sample[i] * sample[i];
+            variance_emp = variance_emp / (double)N;
+            variance_emp -= average_emp * average_emp;
+        }
+
+        double ksi(int i)
+        {
+            int n = i + 1;
+            double ksi = 0;
+            for (int k = 0; k < n; k++)
+            {
+                ksi += r.NextDouble();
+            }
+            ksi -= n / 2;
+            ksi = ksi * (Math.Sqrt(12 / n));
+            return ksi;
+        }
+
+        private void b_start_Click(object sender, EventArgs e)
+        {
+            double min, max;
+            double delta;
+            chart1.Series[0]["PointWidth"] = "1";
+            N = Convert.ToInt32(textBox3.Text);
+            sample = new double[N];
+            average = Convert.ToDouble(textBox1.Text);
+            variance = Convert.ToDouble(textBox2.Text);
+
+            k = (int)Math.Floor(Math.Log(N) / Math.Log(2) + 1);
+
+            for (int i = 0; i < N; i++)
+            {
+                sample[i] = Math.Sqrt(variance) * ksi(i) + average;
+            }
+
+            counter = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                counter[i] = 0;
+            }
+            Fr = new double[k];
+            X = new double[k];
+
+            min = sample.Min(); 
+            max = sample.Max();
+            delta = (max - min) / k;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 1; j <= k; j++)
+                {
+                    if (sample[i] < min + j * delta) 
+                    { 
+                        counter[j - 1]++; 
+                        break; 
+                    }
+                }
+            }
+            for (int i = 0; i < k; i++) Fr[i] = counter[i] / (double)N;
+
+            average_variance(sample);
+            av.Text = average_emp.ToString("F5");
+            var.Text = variance_emp.ToString("F5");
+            chart1.Series[0].Points.Clear();
+            for (int i = 0; i < k; i++) chart1.Series[0].Points.AddXY(X[i], Fr[i]);
+
+        }
+    }
+}
